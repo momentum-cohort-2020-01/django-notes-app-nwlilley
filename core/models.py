f78e833e4ad105c0636c92c1900f260c45d88596
nwlilley <nathaniel.lilley@gmail.com>
2020-02-27 12:12:38 -0500

    add Tag class

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Note(models.Model):
@@ -10,4 +11,9 @@ class Note(models.Model):
 
     def __str__(self):
         return f"Note Title: {self.title} Body: {self.body} Date added: {self.created_at}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    slug = slugify(name)
+
 
